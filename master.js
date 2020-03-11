e5a562d79965f94f31d92953f8d7d81f50df7ce4
Mark den Toom <59030690+MarkdenToom@users.noreply.github.com>
2020-03-11 20:22:15 +0100

    Update master.js
    Removed redundant code

--- master.js
+++ master.js
@@ -2,14 +2,7 @@ console.log("TicTacToe.js connected")
 
 cells = document.querySelectorAll('td')
 restart = document.querySelector('#Restart');
-// two = document.querySelector('#2')
-// three = document.querySelector('#3')
-// four = document.querySelector('#4')
-// five = document.querySelector('#5')
-// six = document.querySelector('#6')
-// seven = document.querySelector('#7')
-// eight = document.querySelector('#8')
-// nine = document.querySelector('#9')
+
 // Clear Squares
 restart.addEventListener('click',function(){
   for (var i = 0; i < cells.length; i++) {
@@ -17,6 +10,7 @@ restart.addEventListener('click',function(){
   }
 })
 
+// Click event
 for (var i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click', function(){
       if (this.textContent == "X") {
